88306e0af72096c1b668909dee9560a1a6f049b5
XiangGao2676922 <2676922g@student.gla.ac.uk>
2022-08-04 20:05:48 +0100

    modify gamewin

--- src/com/gx/GameWin.java
+++ src/com/gx/GameWin.java
@@ -1,10 +1,20 @@
 package com.gx;
 
+import com.gx.obj.Puzzle;
+
 import javax.swing.*;
 import java.awt.*;
 
 
 public class GameWin extends JFrame{
+
+    int cnt=0;
+
+    Puzzle[] puzzle = new Puzzle[8];
+
+    JButton left, right, above, below;
+    JButton restart = new JButton("Restart");
+
     void launch(){
         this.setVisible(true);
         this.setSize(900,900);
@@ -14,7 +24,7 @@ public class GameWin extends JFrame{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    @Override
+/*    @Override
     public void paint(Graphics g) {
         g.setColor(Color.CYAN);
         g.fillRect(0,0,900,900);
@@ -25,10 +35,10 @@ public class GameWin extends JFrame{
         }
         for (int i = 0; i < 3; i++) {
             g.drawLine(i*300,0,i*300,900);
-        }
-    }
-
-
-
+        }*/
 }
+
+
+
+
 

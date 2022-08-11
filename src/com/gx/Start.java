26f3bb80b3acd50048e9f968d4d68764de8d2e9c
XiangGao2676922 <2676922g@student.gla.ac.uk>
2022-08-11 01:43:09 +0100

    add theme

--- src/com/gx/Start.java
+++ src/com/gx/Start.java
@@ -1,8 +1,10 @@
 package com.gx;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+
 public class Start {
     public static  void  main(String[]  args) {
-
+        FlatDarkLaf.setup();
         new GameWin();  //The startup class and the game main body are separated
     }
 }
